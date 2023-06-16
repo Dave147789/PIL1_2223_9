5355b81d60f779d861cc0f2e97005b6adc994c8d
Lheureux <daveazagba@gmail.com>
2023-06-16 13:35:48 +0100

    reorganisation . Tous les fichier sont présent , chercher vous trouverez

--- BASE DE DONNES.sql
+++ BASE DE DONNES.sql
@@ -1,61 +1,36 @@
--- Table pour les utilisateurs
-CREATE TABLE utilisateurs (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  nom VARCHAR(255) NOT NULL,
-  prenom VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  mot_de_passe VARCHAR(255) NOT NULL,
-  role ENUM('etudiant', 'administration') NOT NULL
+CREATE TABLE Utilisateurs (
+  IDUtilisateur INT PRIMARY KEY AUTO_INCREMENT,
+  Nom VARCHAR(255),
+  Prenom VARCHAR(255),
+  Email VARCHAR(255),
+  MotDePasse VARCHAR(255)
 );
 
--- Table pour les promotions
-CREATE TABLE promotions (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  nom VARCHAR(255) NOT NULL
+CREATE TABLE Filieres (
+  IDFiliere INT PRIMARY KEY AUTO_INCREMENT,
+  NomFiliere VARCHAR(255)
 );
 
--- Table pour les filières
-CREATE TABLE filieres (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  nom VARCHAR(255) NOT NULL
+CREATE TABLE Groupes (
+  IDGroupe INT PRIMARY KEY AUTO_INCREMENT,
+  IDFiliere INT,
+  NomGroupe VARCHAR(255),
+  FOREIGN KEY (IDFiliere) REFERENCES Filieres(IDFiliere)
 );
 
--- Table pour les groupes
-CREATE TABLE groupes (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  nom VARCHAR(255) NOT NULL,
-  id_promotion INT,
-  id_filiere INT,
-  FOREIGN KEY (id_promotion) REFERENCES promotions(id),
-  FOREIGN KEY (id_filiere) REFERENCES filieres(id)
+CREATE TABLE Enseignements (
+  IDEnseignement INT PRIMARY KEY AUTO_INCREMENT,
+  NomEnseignement VARCHAR(255)
 );
 
--- Table pour les enseignements
-CREATE TABLE enseignements (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  nom VARCHAR(255) NOT NULL,
-  id_filiere INT,
-  FOREIGN KEY (id_filiere) REFERENCES filieres(id)
-);
-
--- Table pour les cours
-CREATE TABLE cours (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  id_enseignement INT,
-  id_groupe INT,
-  id_enseignant INT,
-  id_salle INT,
-  jour_semaine ENUM('lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi') NOT NULL,
-  heure_debut TIME NOT NULL,
-  heure_fin TIME NOT NULL,
-  FOREIGN KEY (id_enseignement) REFERENCES enseignements(id),
-  FOREIGN KEY (id_groupe) REFERENCES groupes(id),
-  FOREIGN KEY (id_enseignant) REFERENCES utilisateurs(id),
-  FOREIGN KEY (id_salle) REFERENCES salles(id)
-);
-
--- Table pour les salles
-CREATE TABLE salles (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  nom VARCHAR(255) NOT NULL
+CREATE TABLE EmploisDuTemps (
+  IDEmploiDuTemps INT PRIMARY KEY AUTO_INCREMENT,
+  IDGroupe INT,
+  IDEnseignement INT,
+  Jour VARCHAR(255),
+  HeureDebut TIME,
+  HeureFin TIME,
+  Salle VARCHAR(255),
+  FOREIGN KEY (IDGroupe) REFERENCES Groupes(IDGroupe),
+  FOREIGN KEY (IDEnseignement) REFERENCES Enseignements(IDEnseignement)
 );
